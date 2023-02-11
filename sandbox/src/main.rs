3018f546fcf74d5dfa1cad2c84032c5582de11dd
Takatomo Ezo <takatomo@e20.jp>
2023-02-11 17:03:09 +0900

    sandbox: channel with multi threads

--- sandbox/src/main.rs
+++ sandbox/src/main.rs
@@ -1,9 +1,40 @@
+use std::sync::mpsc;
+use std::thread;
+use std::time::Duration;
+
 fn main() {
-    let mut i = 1;
-    let ten_millis = std::time::Duration::from_millis(100);
-    loop {
-        i *= 10;
-        println!("{}", i);
-        std::thread::sleep(ten_millis);
+    let (tx, rx) = mpsc::channel();
+
+    let tx1 = tx.clone();
+    thread::spawn(move || {
+        let vals = vec![
+            String::from("hi"),
+            String::from("from"),
+            String::from("the"),
+            String::from("thread"),
+        ];
+
+        for val in vals {
+            tx1.send(val).unwrap();
+            thread::sleep(Duration::from_secs(1));
+        }
+    });
+
+    thread::spawn(move || {
+        let vals = vec![
+            String::from("more"),
+            String::from("messages"),
+            String::from("for"),
+            String::from("you"),
+        ];
+
+        for val in vals {
+            tx.send(val).unwrap();
+            thread::sleep(Duration::from_secs(1));
+        }
+    });
+
+    for received in rx {
+        println!("Got: {}", received);
     }
 }
