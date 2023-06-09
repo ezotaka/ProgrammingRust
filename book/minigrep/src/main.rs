deb8d3dece3fcbd9453888ed062d018b6cecc9b1
Takatomo Ezo <takatomo@e20.jp>
2023-06-09 14:15:46 +0900

    now new returns Result

--- book/minigrep/src/main.rs
+++ book/minigrep/src/main.rs
@@ -1,10 +1,15 @@
+#[allow(unused_imports)]
 use std::io::prelude::*;
+use std::process;
 use std::{env, fs::File, io::Read};
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let config = Config::new(&args);
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {}", err);
+        process::exit(1);
+    });
 
     let mut f = File::open(config.filename).expect("file not found");
 
@@ -15,14 +20,18 @@ fn main() {
     println!("contents:{}", contents);
 }
 
+#[allow(dead_code)]
 struct Config {
     query: String,
     filename: String,
 }
 
 impl Config {
-    fn new(args: &[String]) -> Config {
+    fn new(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            return Err("not enough args");
+        }
         let (query, filename) = (args[1].clone(), args[2].clone());
-        Config { query, filename }
+        Ok(Config { query, filename })
     }
 }
