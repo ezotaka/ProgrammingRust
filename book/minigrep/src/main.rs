45a8f5b7ae92627c413438a3728929b6a9ce0483
Takatomo Ezo <takatomo@e20.jp>
2023-06-09 14:04:02 +0900

    add Config struct

--- book/minigrep/src/main.rs
+++ book/minigrep/src/main.rs
@@ -4,15 +4,25 @@ use std::{env, fs::File, io::Read};
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let (query, filename) = (&args[1], &args[2]);
-    // print query and filename
-    println!("{}, {}", query, filename);
+    let config = Config::new(&args);
 
-    let mut f = File::open(filename).expect("file not found");
+    let mut f = File::open(config.filename).expect("file not found");
 
     let mut contents = String::new();
     f.read_to_string(&mut contents)
         .expect("something went wrong");
 
     println!("contents:{}", contents);
+}
+
+struct Config {
+    query: String,
+    filename: String,
+}
+
+impl Config {
+    fn new(args: &[String]) -> Config {
+        let (query, filename) = (args[1].clone(), args[2].clone());
+        Config { query, filename }
+    }
 }
