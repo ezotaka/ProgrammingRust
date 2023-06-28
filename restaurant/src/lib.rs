37480f64134a0f4d3be404c5ff38ec7646ad92b9
Takatomo Ezo <takatomo@e20.jp>
2023-06-28 22:42:06 +0900

    restaurant modules are separated as files

--- restaurant/src/lib.rs
+++ restaurant/src/lib.rs
@@ -1,14 +1,4 @@
-pub mod front_of_house {
-    pub mod hosting {
-        pub fn add_to_waitlist() {}
-        fn seat_at_tble() {}
-    }
-    pub mod serving {
-        fn take_order() {}
-        fn serve_order() {}
-        fn take_payment() {}
-    }
-}
+pub mod front_of_house;
 
 pub mod back_of_house {
     pub struct Breakfast {
@@ -31,8 +21,12 @@ pub mod back_of_house {
         Salad,
     }
 }
+pub use crate::front_of_house::hosting;
+
 pub fn eat_at_restaurant() {
     let mut meal = back_of_house::Breakfast::summer("Rye");
     meal.toast = String::from("Wheat");
     println!("I'd like {} toast please", meal.toast);
+
+    hosting::add_to_waitlist();
 }
