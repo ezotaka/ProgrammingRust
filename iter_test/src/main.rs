b216a21beaa2d772fb50dd8776674df7a46a7c11
Takatomo Ezo <takatomo@e20.jp>
2023-07-19 17:54:04 +0900

    Added iterator adaptors

--- iter_test/src/main.rs
+++ iter_test/src/main.rs
@@ -1,6 +1,8 @@
 use std::{
     collections::{BTreeMap, BTreeSet, HashMap},
     fmt::Display,
+    str::FromStr,
+    vec,
 };
 
 fn triangle(n: i32) -> i32 {
@@ -132,11 +134,113 @@ fn repr_funcs() {
     pi!(std::iter::repeat("hoge").take(5));
 }
 
+fn iter_adaptor() {
+    let v = vec!["a", "1", "b", "c", "xyz", "56.7"];
+    pi!(v.iter().filter_map(|c| f64::from_str(c).ok()));
+    pi!(v.iter().flat_map(|w| w.chars()));
+    pi!(v.iter().map(|w| w.chars()).flatten());
+
+    let message = "\
+aaaaa
+bbbbb
+
+ccccc
+";
+
+    pi!(message.lines().take_while(|l| !l.is_empty()));
+    let iter = v.iter();
+    let mut p_iter = iter.peekable();
+    p_iter.next();
+    p_iter.next();
+    println!("{}", p_iter.peek().unwrap());
+    println!("{}", p_iter.next().unwrap());
+
+    let mut flaky = Flaky(true);
+    println!("{}", flaky.next().unwrap());
+    println!("{}", flaky.next().unwrap_or("None"));
+    println!("{}", flaky.next().unwrap());
+
+    let mut not_flaky = Flaky(true).fuse();
+    println!("{}", not_flaky.next().unwrap());
+    println!("{}", not_flaky.next().unwrap_or("None"));
+    println!("{}", not_flaky.next().unwrap_or("None 2"));
+
+    let mut rev_iter = v.iter();
+    println!("{}", rev_iter.next().unwrap());
+    println!("{}", rev_iter.next_back().unwrap());
+    println!("{}", rev_iter.next().unwrap());
+    println!("{}", rev_iter.next_back().unwrap());
+    println!("{}", rev_iter.next().unwrap());
+    println!("{}", rev_iter.next_back().unwrap());
+    println!("{}", rev_iter.next().unwrap_or(&"Ended"));
+
+    let rev_iter = v.iter().rev();
+    let rev_map = rev_iter.map(|w| w.len());
+    pi!(rev_map.rev());
+
+    let im: HashMap<usize, char> = HashMap::from_iter("abc".chars().enumerate());
+    println!("{:?}", im);
+
+    println!("{:?}", (0..5).zip(10..15).collect::<HashMap<_, _>>());
+    println!("{:?}", vec![(0..5).zip(10..15)]);
+
+    let mut lines = message.lines();
+    let ref_message = lines.by_ref();
+    for m in ref_message.take_while(|l| !l.is_empty()) {
+        println!("{}", m);
+    }
+
+    let dirs = ["N", "E", "S", "W"];
+    let mut spin1 = dirs.iter();
+    let s1 = spin1.next().unwrap();
+    println!("{}", spin1.next().unwrap());
+    println!("{}", spin1.next().unwrap());
+    let mut spin2 = dirs.iter();
+    println!("{}", spin2.next().unwrap());
+    println!("{}", spin2.next().unwrap());
+
+    fizz_buzz(50);
+}
+
+fn fizz_buzz(n: i32) {
+    use std::iter::{once, repeat};
+
+    let fizz = repeat("").take(2).chain(once("fizz")).cycle();
+    let buzz = repeat("").take(4).chain(once("buzz")).cycle();
+    let fizzes_buzzes = fizz.zip(buzz);
+
+    let fizz_buzz = (1..=n).zip(fizzes_buzzes).map(|tuple| match tuple {
+        (i, ("", "")) => i.to_string(),
+        (_, (fizz, buzz)) => format!("{}{}", fizz, buzz),
+    });
+
+    for line in fizz_buzz {
+        println!("{}", line);
+    }
+}
+
+struct Flaky(bool);
+
+impl Iterator for Flaky {
+    type Item = &'static str;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.0 {
+            self.0 = false;
+            Some("no problem")
+        } else {
+            self.0 = true; // D'oh!
+            None
+        }
+    }
+}
+
 fn main() {
     // assert_eq!(triangle(4), 10);
     // into_iter();
     // //from_fn();
     // fibonacci();
     // drain();
-    repr_funcs();
+    // repr_funcs();
+    iter_adaptor();
 }
